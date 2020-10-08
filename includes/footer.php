41cda18f45412bc995341a11aaa6ff5ebf6affce
Achal08 <64346967+Achal08@users.noreply.github.com>
2020-10-08 14:53:52 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,52 @@
+<!-- Site footer -->
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify">Bookworm.com <i>Book Store </i> is a recognized leader in the ecommerce industry, with a steadfast commitment to success and a record of achievement that continues a tradition of delivering excellence.
+
+The company was founded in 2020 by Achal Vaidya to ease the process of ordering various books. From those early days with just one employee, Bookworm Store now has more than 10000 employees, serving thousands of customers.</p>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Categories</h6>
+            <ul class="footer-links">
+              <li><a href="">Novel</a></li>
+              <li><a href="">Fictional</a></li>
+              <li><a href="">Artificial Intelligence</a></li>
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="aboutus.php">About Us</a></li>
+              <li><a href="contact.php">Contact Us</a></li>
+              <li><a href="index.php">Shop</a></li>
+              <li><a href="index.php">Privacy Policy</a></li>
+              <li><a href="products.php">Product</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by 
+         <a href="#">BookWorm</a>.
+            </p>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+            </ul>
+          </div>
+        </div>
+      </div>
+</footer>
